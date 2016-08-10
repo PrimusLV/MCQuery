5c438334802cd094aac6b2d7aa1c754525d794eb
PrimusLV <k.drivnieks@inbox.lv>
2016-08-10 22:09:54 +0300

    Delete pinger.php

--- pinger.php
+++
@@ -1,8 +0,0 @@
-<?php
-require("src/Pinger.php");
-$pinger = new Pinger();
-
-if(isset($_GET['host'])) {
-    var_dump($pinger->ping($_GET['host'], isset($_GET['port']) ? $_GET["port"] : 19132));
-}
-?>
